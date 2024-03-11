7bdb6e0ded1da323ef48e359b9ac3c6bfa96729a
khazaimaArshad <160453468+khazaimaArshad@users.noreply.github.com>
2024-03-11 12:10:23 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+function mybtn(){
+  alert('Enter your name');
+}
+function myfun(){
+  alert('Last Name is optional');
+}
+function mytask(){
+  alert('Email should be valid');
+}
+function mywork(){
+  alert('Your password must be at least 6 characters as well as contain at least one uppercase,one lowercase,and one number');
+}
+function myvision(){
+  alert('Enter your valid phone no');
+}
